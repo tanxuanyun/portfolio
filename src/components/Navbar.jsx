43e05cdb8b3585af43a25352b9eb975ac8af90b1
tanxuanyun <tanxuanyun42@gmail.com>
2025-02-16 13:23:17 +0800

    Base code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,12 +76,36 @@ const Navbar = () => {
           <NavLink link={link} key={link.title} />
         ))}
       </div>
+      {/* LOGO */}
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">Lama</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
+        </Link>
+      </div>
       {/* SOCIAL */}
       <div className="hidden md:flex gap-4 w-1/3">
-        <Link href="https://github.com/tanxuanyun">
+        <Link href="/">
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="https://www.linkedin.com/in/tanxuanyun/">
+        <Link href="/">
+          <Image src="/dribbble.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/instagram.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/pinterest.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
           <Image src="/linkedin.png" alt="" width={24} height={24} />
         </Link>
       </div>
